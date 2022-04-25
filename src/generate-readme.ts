2abfb0235f28fc7b9c0981060a7e57fc2abdf367
Yiming Li <yimingli.cs@gmail.com>
2022-04-25 11:42:21 +0800

    docs: add npm to readme

--- src/generate-readme.ts
+++ src/generate-readme.ts
@@ -112,7 +112,8 @@ ${positions.map(position => `
     const banner = `
 # new-grad-positions
 
-[![LICENSE](https://img.shields.io/github/license/upupming/new-grad-positions?style=flat-square)](https://github.com/upupming/new-grad-positions/blob/main/LICENSE) [![PRs](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](https://github.com/upupming/new-grad-positions/blob/main/src/data.ts) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg?style=flat-square)](https://standardjs.com)
+[![LICENSE](https://img.shields.io/github/license/upupming/new-grad-positions?style=flat-square)](https://github.com/upupming/new-grad-positions/blob/main/LICENSE) [![PRs](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](https://github.com/upupming/new-grad-positions/blob/main/src/data.ts) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg?style=flat-square)](https://standardjs.com) [![npm (tag)](https://img.shields.io/npm/v/new-grad-positions/latest?style=flat-square)](https://www.npmjs.com/package/new-grad-positions)
+[![npm](https://img.shields.io/npm/dm/new-grad-positions?style=flat-square)](https://www.npmjs.com/package/new-grad-positions)
 
 本项目旨在收集互联网公司校招和实习的最新官方招聘信息，帮助在校生消除信息不对称带来的劣势。欢迎贡献 👏🏻，可以直接修改 [src/data.ts](src/data.ts) 来增加数据并提交 PR。
 
