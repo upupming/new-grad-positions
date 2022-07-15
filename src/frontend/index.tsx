2017a3fc97095e64af6493a1b6a447b6f3c3ef83
Kaiyi <Zhang.Kaiyi42@gmail.com>
2022-07-15 23:16:07 +0800

    fix: 修复用户系统使用亮色模式时，默认的 icon 依旧是 ‘月亮’ (#8)
    * feat: 加载 App 时, 网页外观跟随用户系统设置
    
    * fix: 修复用户系统为亮色模式时，icon 依旧是 '月亮' icon

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -13,28 +13,19 @@ import './index.css'
 export type Theme = 'dracula' | 'material'
 
 export interface NavProps {
-  onThemeChange?: (theme: 'dracula' | 'material') => void
+  setTheme: (theme: 'dracula' | 'material') => void
+  theme: Theme
 }
 
-function Nav ({ onThemeChange, }: NavProps) {
+function Nav ({ theme, setTheme, }: NavProps) {
   // Using https://github.com/upupming/tailwindcss-themeable for theme switching
-  const [theme, setTheme] = React.useState<Theme>('dracula')
   return (
     <nav className='bg-themeable-background flex justify-between items-center p-4 border-themeable-selection border-b-width-1px sticky top-0 z-10'>
       <h1 className='text-2xl font-semibold'>new-grad-positions</h1>
       <div className='flex items-center space-x-6'>
         <button
           className='inline-flex text-xl p-2 items-center' onClick={() => {
-            setTheme((theme) => {
-              let newTheme: Theme
-              if (theme === 'dracula') {
-                newTheme = 'material'
-              } else {
-                newTheme = 'dracula'
-              }
-              onThemeChange?.(newTheme)
-              return newTheme
-            })
+            setTheme(theme === 'dracula' ? 'material' : 'dracula')
           }}
         >
           <svg className='nav-icon' preserveAspectRatio='xMidYMid meet' viewBox='0 0 24 24' style={{ display: theme === 'dracula' ? undefined : 'none', }}><path fill='currentColor' d='M20.742 13.045a8.088 8.088 0 0 1-2.077.271c-2.135 0-4.14-.83-5.646-2.336a8.025 8.025 0 0 1-2.064-7.723A1 1 0 0 0 9.73 2.034a10.014 10.014 0 0 0-4.489 2.582c-3.898 3.898-3.898 10.243 0 14.143a9.937 9.937 0 0 0 7.072 2.93a9.93 9.93 0 0 0 7.07-2.929a10.007 10.007 0 0 0 2.583-4.491a1.001 1.001 0 0 0-1.224-1.224zm-2.772 4.301a7.947 7.947 0 0 1-5.656 2.343a7.953 7.953 0 0 1-5.658-2.344c-3.118-3.119-3.118-8.195 0-11.314a7.923 7.923 0 0 1 2.06-1.483a10.027 10.027 0 0 0 2.89 7.848a9.972 9.972 0 0 0 7.848 2.891a8.036 8.036 0 0 1-1.484 2.059z' /></svg>
@@ -74,7 +65,7 @@ function App () {
 
   return (
     <div className={`flex flex-col bg-themeable-background h-screen text-themeable-foreground themeable-${theme}`}>
-      <Nav onThemeChange={setTheme} />
+      <Nav theme={theme} setTheme={setTheme} />
       <PositionsTable positions={processedData.positions as PositionRefined[]} />
       <Footer />
     </div>
