0bad30675a53baf7a665234b992cedc370f3b308
张建宏 <57065248+zjh-nuc-AIOT@users.noreply.github.com>
2022-09-18 21:22:59 +0800

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -371,6 +371,19 @@ export const positions: Position[] = [
     graduationYear: 2023,
     type: PositionType['全职（秋季）'],
     company: {
+      name: '飞腾信息技术有限公司',
+      website: 'https://www.phytium.com.cn/',
+      type: CompanyType.国企,
+    },
+    announcement: {
+      url: 'https://mp.weixin.qq.com/s/Uc-y7ssMcdSs_G-g5MJVfg',
+      date: '2022-09-18',
+    },
+  },
+  {
+    graduationYear: 2023,
+    type: PositionType['全职（秋季）'],
+    company: {
       name: 'Zoom',
       website: 'https://www.zoom.us',
       type: CompanyType.私企,
